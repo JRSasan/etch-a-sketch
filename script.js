8cc6d58c1a7df1672df25bc4ec3a07bb8fbffec2
JRSasan <sasanjeanrae@gmail.com>
2024-06-20 09:28:56 +0800

    Add a slider that updates the grid size from 16 up to 100

--- script.js
+++ script.js
@@ -1,29 +1,45 @@
 const grid = document.querySelector(".container");
+const gridSizeSlider = document.querySelector(".grid-size-slider");
+
+let gridSize = gridSizeSlider.value;
+
+createGrid(gridSize);
+
+gridSizeSlider.addEventListener("input", updateGrid)
+
+function createGrid(gridSize) {
+    
+    for (let i = 0; i < gridSize; i++) {
+        for (let j = 0; j < gridSize; j++) {
+            const cell = document.createElement("div");
+            cell.classList.add("cell")
+            cell.style.height = `${400/gridSize}px`;
+            cell.style.width = `${400/gridSize}px`;
+            cell.addEventListener("mousedown", fillToBlack);
+            cell.addEventListener("mouseover", fillToBlack);
+    
+            grid.appendChild(cell);
+        }
+    }
+
+}
+
+function removeGrid() {
+
+    grid.innerHTML = ""
+}
+
+function updateGrid(e) {
+
+    removeGrid();
+    let updatedGridSize = e.target.value;
+    createGrid(updatedGridSize);
+
+}
+
 let mouseDown = false;
 document.body.onmousedown = () => (mouseDown = true);
 document.body.onmouseup = () => (mouseDown = false);
-
-
-for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++) {
-        const cell = document.createElement("div");
-        cell.classList.add("cell")
-        cell.style.height = `${400/16}px`;
-        cell.style.width = `${400/16}px`;
-
-        grid.appendChild(cell);
-    }
-}
-
-const gridCells = document.querySelectorAll(".cell");
-
-gridCells.forEach((gridCell) => {
-
-    gridCell.addEventListener("mousedown", fillToBlack);
-    gridCell.addEventListener("mouseover", fillToBlack);
-
-});
-
 
 function fillToBlack(e) {
     if (e.type === "mouseover" && !mouseDown) {
